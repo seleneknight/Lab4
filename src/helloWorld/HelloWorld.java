edc5385dda88b94590b83d0fef72ed2d9eedfb0b
Selene Knight <selene.knight@pcc.edu>
2024-10-22 09:31:44 -0700

    Added a method that prints 'hello world'

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -1,4 +1,12 @@
 package helloWorld;
 
+/**
+ * @author Selene KNight
+ * @version 10.22.2024
+ */
+
 public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
